dd0dd771bc21f972d49159b8f68ffb4a170f8d33
sky_rush <akashjnu26@gmail.com>
2026-01-02 21:34:49 +0600

    gcd problem is solved

--- codeforce/solve.cpp
+++ codeforce/solve.cpp
@@ -145,9 +145,15 @@ int s(ll x)
     }
     return ans;
 }
+int gcd_int(int a, int b)
+{
+    if (b == 0) return a;
+    return gcd_int(b, a % b);
+}
+
 int lcm(int a, int b)
 {
-    return (a / std::gcd(a, b)) * b;
+    return (a / gcd_int(a, b)) * b;
 }
 
 void solve() {
