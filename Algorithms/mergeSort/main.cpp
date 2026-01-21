1cb5205371b33fc16aabad19eff3e6632bf3a230
sky_rush <akashjnu26@gmail.com>
2026-01-21 16:24:28 +0600

    merge sort

--- Algorithms/mergeSort/main.cpp
+++ Algorithms/mergeSort/main.cpp
@@ -1,1 +1,47 @@
 //merge short used to sort an array of integers with time complexity O(n log n)
+#include <iostream>
+#include <vector>
+using namespace std;
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1);
+    vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    L.push_back(INT_MAX);
+    R.push_back(INT_MAX);
+    int i = 0, j = 0;
+    for (int k = left; k <= right; k++) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+    }
+    
+
+}
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+int main() {
+    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+    mergeSort(arr, 0, arr.size() - 1);
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    return 0;
+}
