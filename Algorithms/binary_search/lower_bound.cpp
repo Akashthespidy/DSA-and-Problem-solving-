b83730e759004251ec8c9bdcd03ec3758f652dc6
sky_rush <akashjnu26@gmail.com>
2026-01-25 19:07:20 +0600

    lower bound

---
+++ Algorithms/binary_search/lower_bound.cpp
@@ -0,0 +1,31 @@
+//lower bound implementation in C++14
+// Finds the index of the first element not less than the target in a sorted array
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;    
+int lowerBound(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size(); 
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] < target) {
+            left = mid + 1; // Move to the right half
+        } 
+        else {
+            right = mid; // Move to the left half
+        }
+    }
+    return left; // Position of the first element not less than target
+}
+int main() {
+    vector<int> arr = {1, 2, 4, 4, 5, 6};
+    int target = 4;
+    int result = lowerBound(arr, target);       
+
+    cout << "Lower bound index for " << target << " is " << result << endl;
+    return 0;
+}
+// it retuens the index if the element is found
+// if not found it returns the index where it can be inserted to keep the array sorted
